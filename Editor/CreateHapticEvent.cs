56aac57499479f39906f90a907909cdb1107e309
Joseph Whitehead <44853302+JosephWhi98@users.noreply.github.com>
2019-09-23 15:46:16 +0100

    Add files via upload

---
+++ Editor/CreateHapticEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CreateHapticEvent
+{
+    [MenuItem("Assets/Create/Haptic Event")]
+    public static void CreateMyAsset()
+    {
+        HapticEvent asset = ScriptableObject.CreateInstance<HapticEvent>();
+
+        AssetDatabase.CreateAsset(asset, "Assets/NewHapticEvent.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = asset;
+    }
+}
